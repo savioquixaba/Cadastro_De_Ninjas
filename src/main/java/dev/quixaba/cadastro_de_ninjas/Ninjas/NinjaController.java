ba47ab155646570f423fc319b5d3043a572b1c4e
savioquixaba <savioquixaba1@hotmail.com>
2026-03-01 18:01:55 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaController.java
@@ -1,9 +1,6 @@
 package dev.quixaba.cadastro_de_ninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -14,6 +11,30 @@ public class NinjaController {
     public String helloWorld(){
         return "Olá Mundo meu primeiro Hello-World em Springboot";
     }
-    
-    
+
+    //Adicionar ninja (Create)
+    @PostMapping("/adicionar")
+    public String adicionarNinja(){
+        return "ninja Criado";
+    }
+    //Mostrar todos os ninjas(READ)
+    @GetMapping("/todos")
+    public String mostrarNinja(){
+        return "lista de ninjas";
+    }
+    //Mostrar Ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjaPorId(){
+        return "mostrar ninja por id";
+    }
+    //Alterar dados dos ninjas
+    @PutMapping("/alterar")
+    public String alterarNinja(){
+        return "ninja alterado";
+    }
+    //Deletar Ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "ninja deletado";
+    }
 }
