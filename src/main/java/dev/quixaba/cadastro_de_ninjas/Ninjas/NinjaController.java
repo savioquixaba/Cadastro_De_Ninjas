9ffb788be02f8803178b509b63083a2c05a7f8fc
savioquixaba <savioquixaba1@hotmail.com>
2026-03-05 17:31:23 -0300

    feat: Inclui DTO como abstração para minha entidade na camada de service e controller

--- src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaController.java
@@ -22,7 +22,8 @@ public class NinjaController {
 
     //Adicionar ninja (Create)
     @PostMapping("/criar")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninja){
+    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja){
+
         return ninjaService.criarNinja(ninja);
     }
 
--- src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaService.java
+++ src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaService.java
@@ -9,9 +9,11 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     public List<NinjaModel> listarNinjas(){
@@ -25,8 +27,10 @@ public class NinjaService {
 
     }
 
-    public NinjaModel criarNinja(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
 
