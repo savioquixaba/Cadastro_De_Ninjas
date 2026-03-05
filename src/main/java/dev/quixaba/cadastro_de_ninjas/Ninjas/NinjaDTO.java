de0c9f2a06980b0e177661ffaaf19176acb885b1
savioquixaba <savioquixaba1@hotmail.com>
2026-03-05 17:28:41 -0300

    chore: inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/quixaba/cadastro_de_ninjas/Ninjas/NinjaDTO.java
@@ -1,5 +1,22 @@
 package dev.quixaba.cadastro_de_ninjas.Ninjas;
 
+import dev.quixaba.cadastro_de_ninjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
+
 
 }
